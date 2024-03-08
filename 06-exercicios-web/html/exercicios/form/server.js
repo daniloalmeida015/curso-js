a96d6d6ccfbbb2d0989e3e6750da6156c327960e
Danilo Almeida da Silva <daniloalmeida_@outlook.com.br>
2024-03-08 16:32:45 -0300

    alterações

--- 06-exercicios-web/html/exercicios/form/server.js
+++ 06-exercicios-web/html/exercicios/form/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+// faz um 'parser' no corpo da requisição request e joga dentro do 'request.body'
+// quando eu acessar 'request.body' vou ter acesso a todos os dados do fomulario que eu submeti
 const bodyParser = require('body-parser')
 
+// associando o bodyparser
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// atendendo a url /usuarios usando método post
+// atendendo uma requisição do tipo POST em cima da url /usuarios
 app.post('/usuarios', (req, resp) => {
+    // req.body pra ver o que chegou no body da requisição
     console.log(req.body)
     console.log(req.query)
     resp.send('<h1>Parabéns. Usuário Incluido!!!</h1>')
@@ -13,7 +19,10 @@ app.post('/usuarios', (req, resp) => {
 app.post('/usuarios/:id', (req, resp) => {
     console.log(req.params.id)
     console.log(req.body)
+    // envia a resposta
     resp.send('<h1>Parabéns. Usuário Alterado!!!</h1>')
 })
 
-app.listen(3003)
+// informa a porta que ficará escutando
+//mesma porta informada no form
+app.listen(3003) 
