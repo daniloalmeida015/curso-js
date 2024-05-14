713f36cb7d4918c1b7c4952965ea15878e36ec79
Danilo Almeida da Silva <daniloalmeida_@outlook.com.br>
2024-05-14 17:51:00 -0300

    xmlHTTPrequest

---
+++ 06-exercicios-web/ajax/server.js
@@ -0,0 +1,117 @@
+//responsavel por fazer o parser do corpo da requisição
+//se eu fizer um 'request' e mandar dados dentro dele ele vai interpretar
+// estes dados e deixar esses dados prontos para serem manipulados
+const bodyParser = require('body-parser');
+
+//importando o servidor 'express'
+const express = require('express');
+
+
+//instanciar o 'express' e coloca-lo em uma variavel
+const app = express();
+
+/*  MIDDLEWARES
+
+    definindo os 'middlewares' que vão ser aplicados sempre que acontecer uma requisição
+    'middlewares' nada mais é que uma função que será executada quando uma determinada requisição chegar
+    pode ser um middleware aplicado a algum determinado padrão de URL
+    ou pode ser aplicado para todas as requisições que chegarem ao servidor
+
+ */
+
+    // static('.') : dentro da pasta atual sirva os arquivos estaticos
+        app.use(express.static('.'));
+
+    //bodyParser
+
+        // se vier algum formato a partir de SUBMIT de formulario, lê os dados e transforma em objeto
+        // vai ser aplicado para qualquer requisição que chegar no servidor
+        app.use(bodyParser.urlencoded({extended: true}));
+        // app.use('/teste',bodyParser.urlencoded({extended: true})); se usasse assim seria apenas para o /teste
+
+        //se vier um JSON dentro do body da requisição vai transformar o json para objeto
+        app.use(bodyParser.json());
+
+
+        // quando vier uma requisição do tipo GET na url '/teste': chame essa função
+        app.get('/teste', (requisicao, resposta) => resposta.send('OK')); //função vai retornar 'ok'
+
+       
+        // upload usando o xmlHTTPRequest
+        //importar o 'multer' para interpretar o formulario que vem o arquivo do upload
+        const multer = require('multer');
+
+
+        //criar uma configuração usada par a pasta que vai salvar os arquivos e para personalizar o nome do arquivo
+        const storage = multer.diskStorage({
+            destino: function(requisicao, arquivo, callback){
+                callback(null, './upload'); //'./upload' pasta de destino. No caso a pasta 'upload'
+            },
+            nomeArquivo: function (requisicao, arquivo, callback){
+                let dataAtual = Date.now();
+                let nomeAtual = arquivo.nomeOriginal;
+                let nomeNovo = `${dataAtual}_${nomeAtual}`;
+
+
+                callback(null, nomeNovo );
+            }
+        })
+
+
+        //vai interpretar o arquivo que vai vir a partir da requisição ajax com o nome 'arquivo'
+        // O método single('arquivo') indica que será permitido apenas um arquivo ser enviado através do formulário com o nome 'arquivo'.
+        // Isso significa que o servidor esperará receber um arquivo com o campo de formulário chamado 'arquivo'.
+        const upload = multer({storage}).single('arquivo');
+
+
+
+
+        //requisição via 'post'
+        app.post('/upload', (req, res)=> {
+            upload(req, res, err =>{
+                if(err){
+                    return res.end('Ocorreu um erro!');
+                }
+
+                res.end('Concuido com sucesso!');
+            })
+        });
+
+
+
+
+
+
+
+
+
+        const porta = 3000;
+        //startar o servidor
+        app.listen(porta, () => console.log(`Servidor executando na porta: ${porta}`));
+
+        //'npm start' no terminal: vai rodar o script 'start' no 'package.json' que esta definido para rodar o 'server.js'
+
+
+        // SE A PORTA ESTIVER EM USO????????????????
+        /* 
+        Para verificar se uma porta específica está em uso no Windows, você pode usar o comando netstat em conjunto
+         com alguns parâmetros adicionais para filtrar as informações exibidas. Por exemplo, se você quiser verificar se 
+         a porta 80 está em uso, você pode fazer o seguinte:
+
+            Abra o Prompt de Comando.
+            Digite o seguinte comando e pressione Enter: 
+            netstat -ano | findstr :80
+
+            Este comando irá filtrar a saída do netstat para exibir apenas as linhas que contenham a porta 80. 
+            Se houver alguma conexão utilizando a porta 80, ela será listada junto com o PID (Identificador de Processo) correspondente.
+
+
+            Depois de identificar o PID do processo, você pode encerrá-lo usando o comando taskkill. 
+            Execute o seguinte comando no Prompt de Comando como administrador, substituindo <PID> pelo 
+            PID do processo que você deseja encerrar:
+
+            taskkill /PID <PID> /F
+
+            O parâmetro /F força o encerramento do processo. 
+        */
+
